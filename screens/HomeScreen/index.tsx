b1f5f7cacdf00b651be854c7da0f4776b751a5e6
Jackson Smith <jackson.96@gmail.com>
2024-10-13 15:44:08 -0300

    remove not used component

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,7 +1,6 @@
-// screens/HomeScreen.tsx
+import InvestmentCard from '@/components/InvestmentCard';
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import InvestmentCard from '../components/InvestmentCard';
+import { Text, StyleSheet, ScrollView } from 'react-native';
 
 const HomeScreen: React.FC = () => {
   return (
